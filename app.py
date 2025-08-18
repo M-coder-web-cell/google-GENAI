7794851d09beaa571c7327e2e52ac969d8c00e74
HackRogue <mazumdarh549@gmail.com>
2025-08-18 22:10:13 +0530

    Some minor fixes and activations

--- app.py
+++ app.py
@@ -10,28 +10,31 @@ load_dotenv()
 
 genaiClient = genai.Client(api_key=os.environ.get("GEMINI-API-KEY"))
 
-@app.route("/generatestory", methods =['POST'])
-def ReceivePrompt():
-    try:
-        #request.is_json checks if content type is application/json
-        if not request.is_json:
-            return jsonify({"error": "Request must be JSON"}), 400
-        
-        requestBody = request.json
-        UserPrompt = requestBody['prompt']
-        story = Sg.generateStory(UserPrompt)
+def main():
+    @app.route("/generatestory", methods =['POST'])
+    def ReceivePrompt():
+        try:
+            #request.is_json checks if content type is application/json
+            if not request.is_json:
+                return jsonify({"error": "Request must be JSON"}), 400
 
-        return jsonify({
-            "status": "Story Generated Successfully",
-            "story": story
-        }), 200
+            requestBody = request.json
+            UserPrompt = requestBody['prompt']
+            story = Sg.generateStory(UserPrompt)
 
-    except Exception as e:
-        return jsonify({
-            "Status" : f"An Internal server error has occured",
-            "error": str(e)}), 500
+            return jsonify({
+                "status": "Story Generated Successfully",
+                "story": story
+            }), 200
+
+        except Exception as e:
+            return jsonify({
+                "Status" : f"An Internal server error has occured",
+                "error": str(e)}), 500
 
 
 if __name__ == "__main__":
+    main()
     app.run(debug=True)
+    
 
