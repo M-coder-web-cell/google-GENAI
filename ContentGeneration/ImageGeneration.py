423391b8762dc5c4def98629dc31b53fa803618f
HackRogue <mazumdarh549@gmail.com>
2025-08-18 21:14:03 +0530

    changed the file structure a bit, shifted from multiple text files to one json file

--- ContentGeneration/ImageGeneration.py
+++ ContentGeneration/ImageGeneration.py
@@ -1,16 +1,17 @@
 from google.genai import types
 from app import genaiClient as client
+import json
 
 def createImageGenerationPrompt( rawImgPrompt : str):
     with open("systemInstructionforImage.txt", "r") as file:
-        systemInstructionforImage = file.read()
+        systemInstructionforImage = json.load(file)
 
     response = client.models.generate_content(
         model ="gemini-1.5-flash",
         contents = rawImgPrompt,
         config=types.GenerateContentConfig(
             thinking_config=types.ThinkingConfig(thinking_budget=0),
-            system_instruction = systemInstructionforImage,
+            system_instruction = systemInstructionforImage['image'],
             response_mime_type = "application/json",
             response_schema={
                 "type": "OBJECT",
--- ContentGeneration/StoryGeneration.py
+++ ContentGeneration/StoryGeneration.py
@@ -6,15 +6,15 @@ import json
 
 
 def generateStory( UserPrompt : str):
-    with open('ContentGeneration/systemInstruction.txt', "r") as file:
-        systemInstruction = file.read()
+    with open('ContentGeneration/SystemInstruction.json', "r") as file:
+        system_instruction_data = json.load(file)
 
     response = client.models.generate_content(
         model ="gemini-1.5-flash",
         contents = UserPrompt,
         config=types.GenerateContentConfig(
             thinking_config=types.ThinkingConfig(thinking_budget=0),
-            system_instruction = systemInstruction,
+            system_instruction = system_instruction_data["story"],
             response_mime_type = "application/json",
             response_schema={
                 "type": "OBJECT",
